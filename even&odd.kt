950168fde1b75edffe95e11bc21c4cfa01b10d36
SakshamSwarup <sakshamswarup2430@gmail.com>
2023-07-31 13:02:00 +0530

    First commit

---
+++ even&odd.kt
@@ -0,0 +1,22 @@
+fun main(){
+
+println(" This is a Odd. ")
+
+for (i in 1..100 step 2){
+    
+  print(" $i")
+
+
+}
+
+println()
+println(" This is a Even.")
+
+for(x in 2..100 step 2 ){
+
+print(" $x")
+
+}
+
+
+}
