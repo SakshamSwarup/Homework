7edf73da41a16cc32e3488bd34037e1202893a9d
SakshamSwarup <sakshamswarup2430@gmail.com>
2023-08-09 09:52:08 +0530

    encapsulation

---
+++ encapsulation.kt
@@ -0,0 +1,26 @@
+fun main(){
+
+val c1 = car()
+
+c1.CarBrand = "Mustand"
+c1.CarYear = 2012
+c1.CarBrand = "Ford"
+
+println(c1.CarBrand)
+
+
+
+
+}
+
+
+class car{
+
+    var CarModel = ""
+    var CarYear = 0
+    var CarBrand = ""
+
+
+
+
+}
