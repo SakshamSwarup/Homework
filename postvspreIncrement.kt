6a2f742d4a510b31958660215a82c833374f7d7a
SakshamSwarup <sakshamswarup2430@gmail.com>
2023-08-12 14:20:47 +0530

    increment

---
+++ postvspreIncrement.kt
@@ -0,0 +1,22 @@
+fun main(){
+
+var a = 10 
+var b = 0
+b = a++  
+
+println("This is a Output of Post Increment.")
+
+println(a)
+println(b)
+
+var p = 10
+var q = 0 
+q = ++p
+
+println("This is a Output of Pre Increment.")
+
+println(p)
+println(q)
+
+
+}
